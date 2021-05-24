e9febf9622b12ca90706e664ce43822e653fd702
Luc Poupard <contact@kloh.ch>
2021-05-24 20:30:37 +0200

    Custom build for VGR
    Made with CKEditor online builder: https://ckeditor.com/ckeditor-5/online-builder

---
+++ build/translations/gu.js
@@ -0,0 +1,1 @@
+(function(t){const i=t["gu"]=t["gu"]||{};i.dictionary=Object.assign(i.dictionary||{},{"Block quote":" વિચાર ટાંકો",Bold:"ઘાટુ - બોલ્ડ્",Code:"",Italic:"ત્રાંસુ - ઇટલિક્",Strikethrough:"",Subscript:"",Superscript:""});i.getPluralForm=function(t){return t!=1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
